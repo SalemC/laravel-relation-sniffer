99ebdc9b135814f0beec614b8c53b830227d1f75
Salem Cresswell <salem@pcfutures.co.uk>
2022-06-23 18:14:20 +0100

    Update GenerateERDiagram.php

--- GenerateERDiagram.php
+++ GenerateERDiagram.php
@@ -9,6 +9,9 @@ use Illuminate\Support\Collection;
 use Illuminate\Console\Command;
 
 use ReflectionClass;
+use Throwable;
+use Closure;
+use Str;
 use DB;
 
 class GenerateERDiagram extends Command {
@@ -25,6 +28,34 @@ class GenerateERDiagram extends Command {
      * @var string
      */
     protected $description = 'Generate an ERD diagram from all models';
+
+    /**
+     * The methods to ignore for their models.
+     *
+     * @var array
+     */
+    private $nonRelationalMethods = [
+        '*' => [
+            'save',
+            'delete',
+            'forceDelete',
+        ],
+
+        //
+    ];
+
+    /**
+     * The map of relations.
+     *
+     * @var \Illuminate\Support\Collection
+     */
+    private $map;
+
+    public function __construct() {
+        parent::__construct();
+
+        $this->map = collect();
+    }
 
     /**
      * Get all the reflected models.
@@ -49,6 +80,10 @@ class GenerateERDiagram extends Command {
             }, collect());
     }
 
+    private function validateRelation(mixed $returnType): bool {
+        return $returnType !== null && (is_a($returnType, Relation::class) || is_subclass_of($returnType, Relation::class));
+    }
+
     /**
      * Execute the console command.
      *
@@ -57,39 +92,86 @@ class GenerateERDiagram extends Command {
     public function handle() {
         DB::beginTransaction();
 
+        $nonRelationalMethods = collect($this->nonRelationalMethods);
+
+        // @todo add methods to ignore for each model
         // @todo ignore pivot models?
-        $this->getReflectedModels()->each(function ($model) {
-            $modelInstance = app()->make($model->getName());
+        $this->getReflectedModels()->each(function ($model) use ($nonRelationalMethods) {
+            $modelClass = $model->getName();
+
+            // Create a copy of any existing 'saving' methods for every model.
+            $savingMethod = Closure::fromCallable([$modelClass, 'saving']);
+
+            $modelInstance = app()->make($modelClass);
+
+            // Prevent database changes occuring while scanning for relations.
+            $modelClass::saving(fn () => false);
 
             $relationMethods = collect($model->getMethods())
-                ->filter(function ($method) use ($modelInstance) {
+                ->filter(function ($method) use ($nonRelationalMethods, $modelClass) {
                     $methodName = $method->getName();
 
-                    if ($method->isStatic()) return false;
-                    if (!$method->isPublic()) return false;
-                    if (str_starts_with($methodName, '__')) return false;
-                    if ($method->getNumberOfParameters() > 0) return false;
+                    $nonRelationalModelMethods = $nonRelationalMethods->get($modelClass);
+                    $nonRelationalWildcards = $nonRelationalMethods->get('*');
+
+                    if (is_array($nonRelationalModelMethods) && in_array($methodName, $nonRelationalModelMethods)) return false;
+                    if (is_array($nonRelationalWildcards) && in_array($methodName, $nonRelationalWildcards)) return false;
+
+                    return true;
+                })->reduce(function ($acc, $method) use ($modelInstance) {
+                    $methodName = $method->getName();
+
+                    if ($method->isStatic()) return $acc;
+                    if (!$method->isPublic()) return $acc;
+                    if (str_starts_with($methodName, '__')) return $acc;
+                    if ($method->getNumberOfParameters() > 0) return $acc;
 
                     $returnType = $method->getReturnType();
 
-                    if ($returnType !== null) {
-                        if (is_a($returnType, Relation::class) || is_subclass_of($returnType, Relation::class)) return true;
+                    if ($returnType !== null && $this->validateRelation($returnType)) {
+                        return $acc->put($methodName, $returnType->getName());
                     }
 
                     try {
-                        $returnType = $modelInstance->$methodName();
-                    } catch (\Exception $e) {
-                        echo "$methodName - failed\n";
-                        echo $e->getMessage() . "\n";
+                        $returnValue = $modelInstance->$methodName();
+
+                        if (!is_object($returnValue)) return $acc;
+
+                        $returnType = get_class($returnValue);
+                    } catch (Throwable $e) {
+                        $this->error($methodName . ' - failed');
+
+                        $errorMessage = $e->getMessage();
+
+                        $this->error($errorMessage);
+
+                        if (Str::startsWith($errorMessage, 'Class "') && Str::endsWith($errorMessage, ' not found')) {
+                            $this->info('- This could be due to an incorrect relation setup');
+                        }
+
+                        $this->newLine();
                     }
 
-                    return $returnType instanceof Relation;
-                });
+                    if (!$this->validateRelation($returnType)) return $acc;
 
-            dd($relationMethods);
+                    return $acc->put($methodName, $returnType);
+                }, collect());
+
+            // Restore the original saving method.
+            $modelClass::saving($savingMethod);
+
+            if (!$this->map->has($modelClass)) $this->map->put($modelClass, collect());
+
+            $relationMethods->each(function ($type, $relation) use ($modelClass, $modelInstance) {
+                $this->map->get($modelClass)->put($relation, [
+                    'table' => $modelInstance->$relation()->getRelated()->getTable(),
+                ]);
+            });
         });
 
         DB::rollBack();
+
+        dd($this->map);
 
         return Command::SUCCESS;
     }
